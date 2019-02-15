b277fcf367b14cd245a0a0237bfde6b20a05dfb4
kor1k <koortttt@gmail.com>
2019-02-15 20:23:05 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// function f() {
+//    let a = confirm('hello world');
+//     if (a === true){
+//         alert('Okay')
+//     } else {
+//         alert('Not Okay!')
+//     }
+// }
+// f();
+//
+alert('asdasdasda');
+jQuery("h1").css("color", "green");
