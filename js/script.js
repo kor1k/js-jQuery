dfd313de8d1482235f0d74f198ba82237dbe6584
kor1k <koortttt@gmail.com>
2019-02-19 21:53:41 +0200

    jQuery practise (modal, fadeIn...)

--- js/script.js
+++ js/script.js
@@ -1,44 +1,56 @@
-// // console.dir($('.item')[3]);
-// // console.dir(document.querySelectorAll('.item'));
+// // // console.dir($('.item')[3]);
+// // // console.dir(document.querySelectorAll('.item'));
+// //
+// // $(document).ready(function () {
+// //     $('.item').click(function () {
+// //         if (!$(this).hasClass('active')){
+// //             console.log($(this));
+// //             $('.active').removeClass('active');
+// //             $(this).addClass('active');
+// //             alert(`index is ${$('.item').index(this)+1}`)
+// //             // $('.item:last-child').addClass('active')
+// //         }
+// //         $('.wrapper').slick({
+// //             slidesToShow: 3
+// //         })
+// //     })
+// // });
+// //
 //
-// $(document).ready(function () {
+//
+//
+// $(document).ready(function(){
 //     $('.item').click(function () {
-//         if (!$(this).hasClass('active')){
+//         if (!$(this).hasClass ('active')){
 //             console.log($(this));
 //             $('.active').removeClass('active');
 //             $(this).addClass('active');
-//             alert(`index is ${$('.item').index(this)+1}`)
-//             // $('.item:last-child').addClass('active')
+//             alert(`Index ${$(".item").index(this)+1}`)
 //         }
-//         $('.wrapper').slick({
-//             slidesToShow: 3
-//         })
 //     })
-// });
 //
-
+//     $('.wrapper').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         dots: true,
+//         autoplay: true,
+//         autoplaySpeed: 1000,
+//     });
+//     $('.masonry').masonry({
+//         // options
+//         itemSelector: '.item',
+//         columnWidth: 110
+//     });
+// });
 
 
 $(document).ready(function(){
-    $('.item').click(function () {
-        if (!$(this).hasClass ('active')){
-            console.log($(this));
-            $('.active').removeClass('active');
-            $(this).addClass('active');
-            alert(`Index ${$(".item").index(this)+1}`)
-        }
+    $('#open-modal').click(function (){
+        $('#modal').addClass('open') ;
+        $('.overlay').addClass('show')}
+    );
+    $('.close, .overlay').click(function () {
+        $('.overlay').removeClass('show')
+        $('#modal').removeClass('open') ;
     })
-
-    $('.wrapper').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 1000,
-    });
-    $('.masonry').masonry({
-        // options
-        itemSelector: '.item',
-        columnWidth: 110
-    });
 });
